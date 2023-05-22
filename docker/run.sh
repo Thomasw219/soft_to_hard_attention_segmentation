65dfacfdfe87df762bd5ff64dd548849356800ba
Manpreet Singh <manprees@andrew.cmu.edu>
2023-05-21 22:31:59 -0400

    fix run

--- docker/run.sh
+++ docker/run.sh
@@ -2,6 +2,7 @@ docker run --name segmentation_learning_env_$1 \
     -it \
     --rm \
     --gpus all \
+    --shm-size 8G \
     --mount type=bind,source="$(pwd)",target=/root/src \
     --dns 8.8.8.8 \
     thomasw219/segmentation_learning:latest
